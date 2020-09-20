870d49b855ecf7e6448df0b34022276909b96103
Zimo <lizimo061@gmail.com>
2020-09-20 21:23:40 +0800

    Add edge factor. Bug when optimization.

--- src/laser_odometry.py
+++ src/laser_odometry.py
@@ -22,21 +22,24 @@ class Odometry:
         self.NEARBY_SCAN = 2.5
         self.OPTIM_ITERATION = 2
         self.DISTORTION = False
-        self.USE_ROBUST_LOSS = True
+        self.USE_ROBUST_LOSS = False
+        self.VOXEL_SIZE = 0.2
     
     def angle_norm(self, angle):
         if angle <= -math.pi:
-            angle += math.pi
+            angle += 2*math.pi
         elif angle > math.pi:
-            angle -= math.pi
+            angle -= 2*math.pi
         return angle
 
     def grab_frame(self, cloud):
         corner_sharp, corner_less, surf_flat, surf_less = self.feature_extractor.feature_extract(cloud)
         if not self.init:
             self.init = True
-            self.surf_last = surf_less
-            self.corner_last = corner_less
+            surf_less_index = self.get_downsample_cloud(surf_less)
+            corner_less_index = self.get_downsample_cloud(corner_less)
+            self.surf_last = surf_less[surf_less_index, :]
+            self.corner_last = corner_less[corner_less_index, :]
         else:
             weight = 1.0
             if self.USE_ROBUST_LOSS:
@@ -45,11 +48,13 @@ class Odometry:
                 loss = None
             for opt_iter in range(self.OPTIM_ITERATION):
                 single_pose_graph = FactorGraph()
-                # corner_points, corner_points_a, corner_points_b = self.get_corner_correspondences(corner_sharp)
-                surf_points, surf_points_a, surf_points_b, surf_points_c = self.get_surf_correspondences(surf_flat)
-                print("Surf points: ", len(surf_points))
-                for i in range(len(surf_points)):
-                    single_pose_graph.add(PlaneFactor(key('p', 0), surf_points[i], surf_points_a[i], surf_points_b[i], surf_points_c[i], weight, loss))
+                corner_points, corner_points_a, corner_points_b = self.get_corner_correspondences(corner_sharp)
+                # surf_points, surf_points_a, surf_points_b, surf_points_c = self.get_surf_correspondences(surf_flat)
+                print("Surf points: ", len(corner_points))
+                # for i in range(len(surf_points)):
+                #     single_pose_graph.add(PlaneFactor(key('p', 0), surf_points[i], surf_points_a[i], surf_points_b[i], surf_points_c[i], weight, loss))
+                for i in range(len(corner_points)):
+                    single_pose_graph.add(EdgeFactor(key('p', 0), corner_points[i], corner_points_a[i], corner_points_b[i], weight, loss))
                 init_pose = Variables()
                 init_pose.add(key('p', 0), self.transform)
 
@@ -83,13 +88,14 @@ class Odometry:
                 closest_ind = ind[0]
                 closest_scan_id = self.corner_last[ind[0], self.RING_INDEX]
                 min_sq_dist2 = self.DIST_THRES
+
                 for j in range(closest_ind+1, self.corner_last.shape[0]):
                     if self.corner_last[j, self.RING_INDEX] <= closest_scan_id:
                         continue
                     if self.corner_last[j, self.RING_INDEX] > closest_scan_id + self.NEARBY_SCAN:
                         break
 
-                    point_sq_dist = np.sum(np.square(self.corner_last[j, self.RING_INDEX] - point_sel))
+                    point_sq_dist = np.sum(np.square(self.corner_last[j, :3].reshape(3,1) - point_sel))
                     if point_sq_dist < min_sq_dist2:
                         min_sq_dist2 = point_sq_dist
                         min_ind2 = j
@@ -100,12 +106,15 @@ class Odometry:
                     if self.corner_last[j, self.RING_INDEX] < closest_scan_id - self.NEARBY_SCAN:
                         break
 
-                    point_sq_dist = np.sum(np.square(self.corner_last[j, self.RING_INDEX] - point_sel))
+                    point_sq_dist = np.sum(np.square(self.corner_last[j, :3].reshape(3,1) - point_sel))
                     if point_sq_dist < min_sq_dist2:
                         min_sq_dist2 = point_sq_dist
                         min_ind2 = j
                 
                 if min_ind2 >= 0:
+                    ab_dist = np.sum(np.square(self.surf_last[min_ind2, :3]-self.surf_last[closest_ind, :3]))
+                    if ab_dist < 1e-3:
+                        continue
                     curr_points.append(corner_sharp[i, :3])
                     points_a.append(self.corner_last[closest_ind, :3])
                     points_b.append(self.corner_last[min_ind2, :3])
@@ -163,6 +172,15 @@ class Odometry:
                     points_c.append(self.surf_last[min_ind3, :3])
 
         return curr_points, points_a, points_b, points_c
+
+    def get_downsample_cloud(self, cloud):
+        o3d_cloud = o3d.geometry.PointCloud()
+        o3d_cloud.points = o3d.utility.Vector3dVector(cloud[:, :3])
+        max_bound = o3d_cloud.get_max_bound() + self.VOXEL_SIZE * 0.5
+        min_bound = o3d_cloud.get_min_bound() - self.VOXEL_SIZE * 0.5
+        out = o3d_cloud.voxel_down_sample_and_trace(self.VOXEL_SIZE, min_bound, max_bound, False)
+        index_ds = [cubic_index[0] for cubic_index in out[2]]
+        return index_ds
 
     def transform_to_start(self, pt):
         s = 1.0
@@ -247,5 +265,80 @@ class PlaneFactor(Factor):
         J_d_transform[0][5] = crx*sry * self.plane_norm[0] - srx * self.plane_norm[1] - crx*cry * self.plane_norm[2]
         return [J_d_transform]
 
+class EdgeFactor(Factor):
+    def __init__(self, key, edge_pt, pt_a, pt_b, weight, loss):
+        Factor.__init__(self, 1, [key], loss)
+        self.edge_p_ = edge_pt.reshape(3,1)
+        self.p_a_ = pt_a.reshape(3,1)
+        self.p_b_ = pt_b.reshape(3,1)
+        self.weight = weight
+    
+    def transform_curr(self, transform):
+        scaled_transform = self.weight * transform
+        rot_mat = get_rotation(scaled_transform[0], scaled_transform[1], scaled_transform[2])
+        translation = scaled_transform[3:6]
+        undistorted_pt = np.transpose(rot_mat).dot(self.edge_p_ - translation.reshape(3,1))
+        return undistorted_pt
+    
+    def copy(self):
+        return EdgeFactor(self.keys()[0], self.edge_p_, self.p_a_, self.p_b_, self.weight, self.lossFunction())
+    
+    def error(self, variables):
+        params = variables.at(self.keys()[0])
+        point_sel = self.transform_curr(params)
+        edge_normal = np.cross((point_sel - self.p_a_), (point_sel - self.p_b_), axis=0)
+        ab_dist = np.linalg.norm(self.p_a_ - self.p_b_)
+        edge_norm = np.linalg.norm(edge_normal)
+        # print(np.array([edge_norm / ab_dist]))
+        return np.array([edge_norm / ab_dist])
 
+    def jacobians(self, variables):
+        params = variables.at(self.keys()[0])
+        point_sel = self.transform_curr(params)
+        edge_normal = np.cross((point_sel - self.p_a_), (point_sel - self.p_b_), axis=0)
+        ab = self.p_a_ - self.p_b_
+        ab_norm = np.linalg.norm(ab)
+        edge_norm = np.linalg.norm(edge_normal)
+        la = (ab[1]*edge_normal[2] + ab[2]*edge_normal[1]) / (ab_norm*edge_norm)
+        lb = -(ab[0]*edge_normal[2] - ab[2]*edge_normal[0]) / (ab_norm*edge_norm)
+        lc = -(ab[0]*edge_normal[1] + ab[1]*edge_normal[0]) / (ab_norm*edge_norm)
+        srx = np.sin(params[0])
+        crx = np.cos(params[0])
+        sry = np.sin(params[1])
+        cry = np.cos(params[1])
+        srz = np.sin(params[2])
+        crz = np.cos(params[2])
+        tx = params[3]
+        ty = params[4]
+        tz = params[5]
 
+        J_d_transform = np.empty([1,6])
+        J_d_transform[0][0] = (-crx*sry*srz*self.edge_p_[0] + crx*crz*sry*self.edge_p_[1] + srx*sry*self.edge_p_[2] \
+                              + tx*crx*sry*srz - ty*crx*crz*sry - tz*srx*sry) * la \
+                              + (srx*srz*self.edge_p_[0] - crz*srx*self.edge_p_[1] + crx*self.edge_p_[2] \
+                              + ty*crz*srx - tz*crx - tx*srx*srz) * lb \
+                              + (crx*cry*srz*self.edge_p_[0] - crx*cry*crz*self.edge_p_[1] - cry*srx*self.edge_p_[2] \
+                              + tz*cry*srx + ty*crx*cry*crz - tx*crx*cry*srz) * lc
+
+        J_d_transform[0][1] = ((-crz*sry - cry*srx*srz)*self.edge_p_[0] \
+                              + (cry*crz*srx - sry*srz)*self.edge_p_[1] - crx*cry*self.edge_p_[2] \
+                              + tx*(crz*sry + cry*srx*srz) + ty*(sry*srz - cry*crz*srx) \
+                              + tz*crx*cry) * la \
+                              + ((cry*crz - srx*sry*srz)*self.edge_p_[0] \
+                              + (cry*srz + crz*srx*sry)*self.edge_p_[1] - crx*sry*self.edge_p_[2] \
+                              + tz*crx*sry - ty*(cry*srz + crz*srx*sry) \
+                              - tx*(cry*crz - srx*sry*srz)) * lc
+
+        J_d_transform[0][2] = ((-cry*srz - crz*srx*sry)*self.edge_p_[0] + (cry*crz - srx*sry*srz)*self.edge_p_[1] \
+                              + tx*(cry*srz + crz*srx*sry) - ty*(cry*crz - srx*sry*srz)) * la \
+                              + (-crx*crz*self.edge_p_[0] - crx*srz*self.edge_p_[1] \
+                              + ty*crx*srz + tx*crx*crz) * lb \
+                              + ((cry*crz*srx - sry*srz)*self.edge_p_[0] + (crz*sry + cry*srx*srz)*self.edge_p_[1] \
+                              + tx*(sry*srz - cry*crz*srx) - ty*(crz*sry + cry*srx*srz)) * lc
+        
+        J_d_transform[0][3] = -(cry*crz - srx*sry*srz) * la + crx*srz * lb \
+                              - (crz*sry + cry*srx*srz) * lc
+        J_d_transform[0][4] = -(cry*srz + crz*srx*sry) * la - crx*crz * lb \
+                              - (sry*srz - cry*crz*srx) * lc
+        J_d_transform[0][5] = crx*sry * la - srx * lb - crx*cry * lc
+        return [J_d_transform]
